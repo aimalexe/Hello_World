aa4a8242e08d119a57137e511660f01824f1f6ef
aimalexe <aimalexe@gmail.com>
2022-06-04 00:12:35 +0500

    CPP added and README updated.

---
+++ 4-CPP/hello_world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "\tHello World!" << endl;
+	cout << "\n\"C++ is designed to allow you to express ideas, but if ou dont have ideas or any\n clue about how to express them, C++ does't offer much help.\"";
+	cout << "\n\t\tBjarne Stroustrup.";
+	return 0;
+}
