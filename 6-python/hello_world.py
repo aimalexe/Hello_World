0cb19bcf91fb12800426deb6150e3c6ef58cd56c
aimalexe <aimalexe@gmail.com>
2022-10-13 22:19:24 +0500

    python is added and readme updated

---
+++ 6-python/hello_world.py
@@ -0,0 +1,11 @@
+
+
+h = "Hello amigos!\nI am starting python...\n"
+el = '\tThe minimalistic design philosophy of Python ' 
+lo = "means that it emphasizes code readability. "
+wo = "It also provides a syntax structure "
+r = "that allows programmers to express concepts in "
+l = "fewer lines of code than in languages such as C++ and Java."
+d = ' Moreover, Python provides the means to write programs that can be scaled up easily.'
+HelloWorld = h + el + lo + wo + r + l + d
+print(HelloWorld)
